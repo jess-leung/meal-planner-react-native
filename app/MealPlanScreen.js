fcdbc52f4d93be49dcc92332b84b4bd547d26829
Jessie Leung <jleu1656@gmail.com>
2018-04-09 13:18:03 +1000

    Add list of days for the plan and button for add meal

--- app/MealPlanScreen.js
+++ app/MealPlanScreen.js
@@ -4,24 +4,40 @@ import React, { Component } from 'react';
 import {
   Text,
   View,
+  FlatList
 } from 'react-native';
-import { Header } from 'react-native-elements';
+import {
+  Header,
+  Card,
+  Button
+} from 'react-native-elements';
 
 export default class MealPlanScreen extends Component<{}> {
-    render() {
-      return (
-        <View>
-          <Header
-            leftComponent={{ icon: 'menu', color: '#fff' }}
-            centerComponent={{ text: 'Meal Planner', style: { color: '#fff' } }}
-          />
-          <Text>
-            Search for houses to buy!
-          </Text>
-          <Text>
-            Search by place-name or postcode.
-          </Text>
-        </View>
-      );
-    }
+  render() {
+    return (
+      <View>
+        <Header
+          leftComponent={{ icon: 'menu', color: '#fff' }}
+          centerComponent={{ text: 'Meal Planner', style: { color: '#fff' } }}
+          backgroundColor='#4CAF50'
+        />
+        <FlatList
+          data={[{ title: 'MONDAY', key: 'monday' }, { title: 'TUESDAY', key: 'tuesday' }, { title: 'WEDNESDAY', key: 'wednesday' },
+          { title: 'THURSDAY', key: 'thursday' }, { title: 'FRIDAY', key: 'friday' }]}
+          renderItem={({ item }) =>
+            <Card title={item.title} >
+              <Text>No meals</Text>
+              <Button
+                buttonStyle={{
+                  backgroundColor: '#4CAF50',
+                  borderColor: 'transparent',
+                  borderWidth: 0,
+                  borderRadius: 5,
+                }}
+                title='ADD MEAL' />
+            </Card>}
+        />
+      </View>
+    );
   }
+}
